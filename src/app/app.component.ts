3615d4061747eb46fe03c2cea728644ba9c79722
Vladislav Grents <vladair_o@hotmail.com>
2022-08-16 00:21:28 +0500

    Add getAll products with api + global error component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { ProductsService } from './services/products.service';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-tutorial';
+export class AppComponent implements OnInit {
+  title: string = 'Products'
+  //products: IProduct[] = []
+  isLoading: boolean = false
+  products$: Observable<IProduct[]>
+
+  constructor(private productsService: ProductsService) {   
+  }
+
+  ngOnInit(): void {
+    this.isLoading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.isLoading = false)
+    )
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.isLoading = false
+    // })
+  }
 }
