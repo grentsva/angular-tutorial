b577b3372b8a03ac207d8cc7a1c60b196a54fcc5
Vladislav Grents <vladair_o@hotmail.com>
2022-09-01 20:11:13 +0500

    Change preloader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ProductComponent } from './components/product/product.component'
-import { GlobalErrorComponent } from './components/global-error/global-error.component'
+import { GlobalErrorComponent } from './components/common/global-error/global-error.component'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { FilterProductsPipe } from './pipes/filter-products.pipe'
 import { ModalComponent } from './components/modal/modal.component'
 import { ProductAddComponent } from './components/product-add/product-add.component'
-import { FocusDirective } from './directives/focus.directive';
-import { ProductPageComponent } from './pages/product-page/product-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { FocusDirective } from './directives/focus.directive'
+import { ProductPageComponent } from './pages/product-page/product-page.component'
+import { AboutPageComponent } from './pages/about-page/about-page.component'
 import { NavigationComponent } from './components/navigation/navigation.component'
+import { PreloaderComponent } from './components/common/preloader/preloader.component'
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { NavigationComponent } from './components/navigation/navigation.componen
     FocusDirective,
     ProductPageComponent,
     AboutPageComponent,
-    NavigationComponent
+    NavigationComponent,
+    PreloaderComponent
   ],
   imports: [
     BrowserModule,
